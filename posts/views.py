3d7ff1ace9bfb50a207ac5505d786b3da4505205
epode4 <epode4@gamil.com>
2023-08-31 14:03:54 +0900

    like 기능

--- posts/views.py
+++ posts/views.py
@@ -52,3 +52,23 @@ def comment_create(request, post_id):
         comment.save()
 
         return redirect('posts:index')
+
+@login_required
+def like(request, post_id):
+    user = request.user
+    post = Post.objects.get(id=post_id)
+
+    # 이미 좋아요 버튼을 누른 경우
+    if user in post.like_users.all():
+        post.like_users.remove(user)
+    # if post in user.like_posts.all():
+    #     user.like_posts.remove(post)
+
+    # 좋아요 버튼을 아직 안 누른 경우
+    else:
+        post.like_users.add(user)
+        # user.like_posts.add(post)
+        
+
+
+    return redirect('posts:index')
